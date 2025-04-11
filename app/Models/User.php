759b33523cb54415b056f0b32d5e48eb8456c222
Janus Helkjær <janus.helkjaer@gmail.com>
2025-04-11 22:57:54 +0200

    filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -57,4 +59,11 @@ class User extends Authenticatable
             ->map(fn (string $name) => Str::of($name)->substr(0, 1))
             ->implode('');
     }
+
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_contains($this->email, 'janus.helkjaer@gmail.com');
+    }
+    
 }
